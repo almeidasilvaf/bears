a40b78efd211880388006f48c747f3afedc788cc
almeidasilvaf <fabricio_almeidasilva@hotmail.com>
2022-05-18 14:52:55 +0200

    Changed GHA workflow to deploy website

--- inst/scripts/create_environment.R
+++ inst/scripts/create_environment.R
@@ -1,4 +1,71 @@
 
+#----Set up conda environemnt management with Herper----------------------------
+library(Herper)
+my_miniconda <- file.path(tempdir(), "miniconda_bears")
+
+#----Create conda environments for each external dependency
+## MultiQC
+multiqc_env <- install_CondaTools("multiqc", "multiqc_env", 
+                                  pathToMiniConda = my_miniconda)
+
+system2("source", args = "env/miniconda_atlas/etc/profile.d/conda.sh")
+system2("conda", args = "init")
+system2("source", args = "~./bashrc")
+
+with_CondaEnv("multiqc_env",
+              system2("multiqc", args = "-h"),
+              pathToMiniConda = my_miniconda)
+
+## FastQC
+fastqc_env <- install_CondaTools("fastqc", "fastqc_env", 
+                                 pathToMiniConda = my_miniconda)
+
+with_CondaEnv("fastqc_env",
+              system2("fastqc", args = "-h"),
+              pathToMiniConda = my_miniconda)
+
+
+## Trimmomatic
+trimmomatic_env <- install_CondaTools("trimmomatic", "trimmomatic_env", 
+                                      pathToMiniConda = my_miniconda)
+
+## SortMeRNA
+sortmerna_env <- install_CondaTools("sortmerna", "sortmerna_env", 
+                                    pathToMiniConda = my_miniconda)
+
+## STAR
+star_env <- install_CondaTools("star", "star_env", 
+                               pathToMiniConda = my_miniconda)
+
+## RSeQC
+rseqc_env <- install_CondaTools("rseqc", "rseqc_env", 
+                                pathToMiniConda = my_miniconda)
+
+## salmon
+salmon_env <- install_CondaTools("salmon", "salmon_env", 
+                                 pathToMiniConda = my_miniconda)
+
+
+## kallisto
+kallisto_env <- install_CondaTools("kallisto", "kallisto_env", 
+                                   pathToMiniConda = my_miniconda)
+
+## Subread
+subread_env <- install_CondaTools("subread", "subread_env", 
+                                  pathToMiniConda = my_miniconda)
+
+## StringTie
+stringtie_env <- install_CondaTools("stringtie", "stringtie_env", 
+                                    pathToMiniConda = my_miniconda)
+
+## TACO
+taco_env <- install_CondaTools("taco", "taco_env", 
+                               pathToMiniConda = my_miniconda)
+
+
+
+
+
 # Create conda environment with herper
 # envname: "bear_env"
 # miniconda_path: my_miniconda
