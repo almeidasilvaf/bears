9350f5cf06cfb5b3568256661b3bbd95d28b423c
almeidasilvaf <fabricio_almeidasilva@hotmail.com>
2022-05-18 15:23:49 +0200

    Added .yml files to create a Conda environment for each external dependency + changed branch name in GHA workflow

--- inst/scripts/create_environment.R
+++ inst/scripts/create_environment.R
@@ -1,6 +1,7 @@
 
 #----Set up conda environemnt management with Herper----------------------------
 library(Herper)
+library(here)
 my_miniconda <- file.path(tempdir(), "miniconda_bears")
 
 #----Create conda environments for each external dependency
@@ -8,95 +9,92 @@ my_miniconda <- file.path(tempdir(), "miniconda_bears")
 multiqc_env <- install_CondaTools("multiqc", "multiqc_env", 
                                   pathToMiniConda = my_miniconda)
 
-system2("source", args = "env/miniconda_atlas/etc/profile.d/conda.sh")
-system2("conda", args = "init")
-system2("source", args = "~./bashrc")
+export_CondaEnv("multiqc_env",
+                yml_export = here("inst", "extdata", "multiqc_env.yml"),
+                my_miniconda)
 
-with_CondaEnv("multiqc_env",
-              system2("multiqc", args = "-h"),
-              pathToMiniConda = my_miniconda)
+# Test that it works
+# with_CondaEnv("multiqc_env",
+#               system2("multiqc", args = "-h"),
+#               pathToMiniConda = my_miniconda)
 
 ## FastQC
 fastqc_env <- install_CondaTools("fastqc", "fastqc_env", 
                                  pathToMiniConda = my_miniconda)
 
-with_CondaEnv("fastqc_env",
-              system2("fastqc", args = "-h"),
-              pathToMiniConda = my_miniconda)
-
+export_CondaEnv("fastqc_env",
+                yml_export = here("inst", "extdata", "fastqc_env.yml"),
+                my_miniconda)
 
 ## Trimmomatic
 trimmomatic_env <- install_CondaTools("trimmomatic", "trimmomatic_env", 
                                       pathToMiniConda = my_miniconda)
 
+export_CondaEnv("trimmomatic_env",
+                yml_export = here("inst", "extdata", "trimmomatic_env.yml"),
+                my_miniconda)
+
 ## SortMeRNA
 sortmerna_env <- install_CondaTools("sortmerna", "sortmerna_env", 
                                     pathToMiniConda = my_miniconda)
+
+export_CondaEnv("sortmerna_env",
+                yml_export = here("inst", "extdata", "sortmerna_env.yml"),
+                my_miniconda)
 
 ## STAR
 star_env <- install_CondaTools("star", "star_env", 
                                pathToMiniConda = my_miniconda)
 
+export_CondaEnv("star_env",
+                yml_export = here("inst", "extdata", "star_env.yml"),
+                my_miniconda)
+
 ## RSeQC
 rseqc_env <- install_CondaTools("rseqc", "rseqc_env", 
                                 pathToMiniConda = my_miniconda)
+
+export_CondaEnv("rseqc_env",
+                yml_export = here("inst", "extdata", "rseqc_env.yml"),
+                my_miniconda)
 
 ## salmon
 salmon_env <- install_CondaTools("salmon", "salmon_env", 
                                  pathToMiniConda = my_miniconda)
 
+export_CondaEnv("salmon_env",
+                yml_export = here("inst", "extdata", "salmon_env.yml"),
+                my_miniconda)
 
 ## kallisto
 kallisto_env <- install_CondaTools("kallisto", "kallisto_env", 
                                    pathToMiniConda = my_miniconda)
 
+export_CondaEnv("kallisto_env",
+                yml_export = here("inst", "extdata", "kallisto_env.yml"),
+                my_miniconda)
+
 ## Subread
 subread_env <- install_CondaTools("subread", "subread_env", 
                                   pathToMiniConda = my_miniconda)
+
+export_CondaEnv("subread_env",
+                yml_export = here("inst", "extdata", "subread_env.yml"),
+                my_miniconda)
+
 
 ## StringTie
 stringtie_env <- install_CondaTools("stringtie", "stringtie_env", 
                                     pathToMiniConda = my_miniconda)
 
+export_CondaEnv("stringtie_env",
+                yml_export = here("inst", "extdata", "stringtie_env.yml"),
+                my_miniconda)
+
 ## TACO
 taco_env <- install_CondaTools("taco", "taco_env", 
                                pathToMiniConda = my_miniconda)
 
-
-
-
-
-# Create conda environment with herper
-# envname: "bear_env"
-# miniconda_path: my_miniconda
-
-
-my_miniconda <- file.path("/Users/almeidasilvaf", "miniconda_herper")
-Herper::install_CondaTools("multiqc", "bear_env", pathToMiniConda = my_miniconda)
-Herper::install_CondaTools("sra-tools==2.10.1", "bear_env", updateEnv = TRUE, 
-                           pathToMiniConda = my_miniconda) # 2.11.0
-Herper::install_CondaTools("fastqc", "bear_env", updateEnv = TRUE, 
-                           pathToMiniConda = my_miniconda)
-Herper::install_CondaTools("trimmomatic", "bear_env", updateEnv = TRUE, 
-                           pathToMiniConda = my_miniconda)
-Herper::install_CondaTools("sortmerna", "bear_env", updateEnv = TRUE, 
-                           pathToMiniConda = my_miniconda)
-Herper::install_CondaTools("star", "bear_env", updateEnv = TRUE, 
-                           pathToMiniConda = my_miniconda)
-Herper::install_CondaTools("rseqc", "bear_env", updateEnv = TRUE, 
-                           pathToMiniConda = my_miniconda)
-# Herper::install_CondaTools("shrimp", "bear_env", channels = "biobuilds",
-#                            updateEnv = TRUE, pathToMiniConda = my_miniconda)
-Herper::install_CondaTools("salmon", "bear_env", updateEnv = TRUE,
-                           pathToMiniConda = my_miniconda)
-Herper::install_CondaTools("kallisto", "bear_env", updateEnv = TRUE,
-                           pathToMiniConda = my_miniconda)
-Herper::install_CondaTools("subread", "bear_env", updateEnv = TRUE,
-                           pathToMiniConda = my_miniconda)
-Herper::install_CondaTools("stringtie", "bear_env", updateEnv = TRUE,
-                           pathToMiniConda = my_miniconda)
-
-# Create new env for TACO
-Herper::install_CondaTools("taco", "taco_env", pathToMiniConda = my_miniconda,
-                           updateEnv = TRUE)
-
+export_CondaEnv("taco_env",
+                yml_export = here("inst", "extdata", "taco_env.yml"),
+                my_miniconda)
