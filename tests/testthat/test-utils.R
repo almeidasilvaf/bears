35edf2ac67026fd5ea427e06c8e72dc7aa1655a6
almeidasilvaf <fabricio_almeidasilva@hotmail.com>
2022-09-22 14:48:58 +0200

    Added command to install Herper from GitHub

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -11,9 +11,11 @@ mappingdir <- system.file("extdata", package = "bears")
 test_that("skip() works", {
     s1 <- skip(platform = "Illumina Hiseq")
     s2 <- skip(platform = "PacBio")
+    s3 <- skip(platform = "PacBio", path = bedpath)
     
     expect_false(s1)
     expect_true(s2)
+    expect_equal(class(s3), "logical")
 })
 
 test_that("gff2bed() converts a GFF file to BED", {
@@ -39,3 +41,40 @@ test_that("infer_strandedness() infers strandedness", {
     
     expect_equal(class(s1), "data.frame")
 })
+
+
+test_that("translate_strandedness() correctly creates params for programs", {
+    
+    t1 <- translate_strandedness("first", layout = "SINGLE")
+    t2 <- translate_strandedness("second", layout = "PAIRED")
+    t3 <- translate_strandedness("unstranded", layout = "SINGLE")
+    t4 <- translate_strandedness("random", layout = "SINGLE")
+    
+    expect_equal(class(t1), "list")
+    expect_equal(class(t2), "list")
+    expect_equal(class(t3), "list")
+    expect_equal(class(t4), "list")
+})
+
+
+test_that("c_createdir() creates a dir if it doesn't exist", {
+    
+    dir <- file.path(tempdir(), paste0("testdir", sample(1:1000, 1)))
+    c <- c_createdir(dir)
+    
+    expect_equal(class(c), "logical")
+})
+
+
+test_that("create_dir_structure() returns a list of paths", {
+    
+    rootdir <- tempdir()
+    ds <- create_dir_structure(rootdir)
+    
+    expect_equal(class(ds), "list")
+    expect_equal(class(ds$fastqdir), "character")
+})
+
+
+
+
